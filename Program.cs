9f2b3c235696fddc11e0e02465ee948f822af563
SofiaTitova <132710963+SofiaTitova@users.noreply.github.com>
2024-04-09 22:18:34 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,201 +1,135 @@
-﻿using bwt_rle;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace burrows_wheeler_transform
+namespace huffman
 {
-    public class FileCompressionRatio
+    public class HeapNode
     {
-        public static double GetCompressionRatio(string filePath1, string filePath2)
+        public char Sym { get; set; }
+        public int Val { get; set; }
+        public HeapNode LeftChild { get; set; }
+        public HeapNode RightChild { get; set; }
+
+        public HeapNode(char sym, int val)
         {
-            FileInfo fileInfo1 = new FileInfo(filePath1);
-            FileInfo fileInfo2 = new FileInfo(filePath2);
-
-            long size1 = fileInfo1.Length;
-            long size2 = fileInfo2.Length;
-
-            if (size1 == 0 || size2 == 0)
-            {
-                throw new ArgumentException("File size cannot be zero.");
-            }
-
-            double ratio = (double)size1 / size2;
-
-            return ratio;
+            Sym = sym;
+            Val = val;
         }
     }
-    internal class Program
+
+    public class Program
     {
-
-        public static int part = 50;
-        static void Main(string[] args)
+        public static void Main()
         {
+            char[] symbols = { 'a', 'b', 'c', 'd', 'e' };
+            int[] vals = { 10, 5, 2, 14, 15 };
+            string[] keys = new string[symbols.Length];
+            char[] chars = new char[symbols.Length];
 
-            string graf = "text.txt"; /*"output2.txt"; //"graph.txt"*/
-            string graf_bwt = "bwt_graf.txt";
-            string graf_res = "resgraph.txt";
-            //bigBWT(graf, graf_bwt);
-            BWTFast bwt = new BWTFast();
-            string wholeText = "";
-            string line = "";
-            using (StreamWriter res = new StreamWriter(graf_bwt)) 
+            Console.WriteLine("Initial conditions");
+            for (int j = 0; j < symbols.Length; j++)
             {
-                using (StreamReader reader = new StreamReader(graf, Encoding.UTF8)) 
-                {
-                    while ((line = reader.ReadLine()) != null) 
-                    {
-                       wholeText+= line;
-                        
-                    }
-                }
-                bwt.GetBWT(wholeText);
-                res.Write(wholeText);
+                Console.WriteLine(symbols[j] + ": " + vals[j]);
             }
-           Console.WriteLine("+");
 
-            rle(graf_bwt , graf_res);
-            double re = FileCompressionRatio.GetCompressionRatio(graf_res, graf);
-            Console.WriteLine(re);
+            HuffCodes(symbols, vals, chars, keys);
+
+            Console.WriteLine("Huffman codes");
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.WriteLine(chars[i] + ": " + keys[i]);
+            }
+
+            string main = "abcde";
+            string res = HcAlg(main, keys, chars);
+            Console.WriteLine("Direct conversion of: " + main + " is " + res);
+            main = RevHcAlg(res, keys, chars);
+            Console.WriteLine("Reverse conversion: " + main);
             Console.ReadKey();
-
         }
-       
-        static void BWT(out string result, string inStr, ref int ind)
+
+        public static void HuffCodes(char[] s, int[] v, char[] chars, string[] keys)
         {
-            result = "";
-            string[] bwt_matrix = new string[inStr.Length];
-            string toAdd;
-            string mainPart;
-            string newStr = inStr;
-            string part;
-            int len = newStr.Length;
-            for (int i = 0; i < len; i++)
+            var minHeap = new SortedSet<HeapNode>(Comparer<HeapNode>.Create((a, b) => a.Val.CompareTo(b.Val)));
+            int i = 0;
+            for (i = 0; i < s.Length; i++)
             {
-                toAdd = inStr.Substring(0, 1);
-                mainPart = inStr.Substring(1, len - 1);
-                inStr = mainPart + toAdd;
-                bwt_matrix[i] = inStr;
-
-            }
-            string[] sorted_bwt = bwt_matrix.OrderBy(word => word).ToArray();
-            for (int i = 0; i < len; i++)
-            {
-                if (sorted_bwt[i] == newStr)
-                {
-                    ind = i;
-                }
-            }
-            for (int i = 0; i < len; i++)
-            {
-                part = sorted_bwt[i].Substring(len - 1);
-                result += part;
+                minHeap.Add(new HeapNode(s[i], v[i]));
             }
 
+            while (minHeap.Count != 1)
+            {
+                var leftChild = minHeap.First();
+                minHeap.Remove(leftChild);
+
+                var rightChild = minHeap.First();
+                minHeap.Remove(rightChild);
+
+                var tmp = new HeapNode('+', (leftChild.Val + rightChild.Val));
+                tmp.LeftChild = leftChild;
+                tmp.RightChild = rightChild;
+
+                minHeap.Add(tmp);
+            }
+
+            i = 0;
+            CodesToArr(minHeap.First(), chars, keys, "", ref i);
         }
-        static void rle(string gr, string grRes)
+
+        public static void CodesToArr(HeapNode root, char[] symb, string[] freq, string str, ref int i)
         {
-            using (StreamReader reader = new StreamReader(gr, Encoding.UTF8)) //чтение построчно
+            if (root == null)
             {
-                using (StreamWriter res = new StreamWriter(grRes, false))
-                {
-                    string line;
-                    string hlp = "";
-                    char currentChar = '0';
-                    while ((line = reader.ReadLine()) != null) // пптриааа
-                    {
-                        int count = 1;
-                        int index = 0;
-
-                        while (index < (line.Length - 2))
-                        {
-                            currentChar = line[index];
-                            if (currentChar == line[index + 1])
-                            {
-                                count++;
-                                index++;
-                            }
-                            else
-                            {
-
-                                if (count == 1)
-                                {
-                                    index++;
-                                    hlp += line[index - 1];
-                                    while (line[index] != line[index - 1] && index != line.Length - 1)
-                                    {
-                                        hlp += line[index];
-                                        index++;
-                                    }
-                                    hlp = hlp.Substring(0, hlp.Length - 1);
-                                    res.Write('&');
-                                    res.Write(hlp);
-                                    res.Write('&');
-                                    hlp = "";
-                                    index--;
-                                }
-                                else
-                                {
-                                    res.Write((char)(count));
-                                    res.Write(currentChar);
-                                    count = 1;
-                                    index++;
-                                }
-                            }
-
-                        }
-
-                        res.Write((char)(count));
-                        res.Write(currentChar);
-                        res.Write('\n');
-                    }
-                }
+                return;
             }
+
+            if (root.Sym != '+')
+            {
+                symb[i] = root.Sym;
+                freq[i] = str;
+                i++;
+            }
+
+            CodesToArr(root.LeftChild, symb, freq, str + "0", ref i);
+            CodesToArr(root.RightChild, symb, freq, str + "1", ref i);
         }
-        static void bigBWT(string inp, string outp)
+
+        public static string HcAlg(string main, string[] keys, char[] chars)
         {
-            string line;
-            using (StreamReader reader = new StreamReader(inp, Encoding.UTF8))
-            using (StreamWriter writer = new StreamWriter(outp))
+            string res = "";
+            for (int i = 0; i < main.Length; i++)
             {
-                while ((line = reader.ReadLine()) != null)
-                {
-                    int len = line.Length;
-                    int k = 0;
-
-                    for (int i = len; i > 0; i -= part)
-                    {
-                        string textPart;
-                        int partLen;
-
-                        if (i - part < 0)
-                        {
-                            textPart = line.Substring(part * k);
-                            partLen = line.Length - part * k;
-                        }
-                        else
-                        {
-                            textPart = line.Substring(part * k, part);
-                            k++;
-                            partLen = part;
-                        }
-                        string result;
-                        int ind = 0;
-                        BWT(out result, textPart, ref ind);
-                        writer.Write(result);
-                        writer.Write((char)(ind + part));
-
-                    }
-                    writer.Write("\n");
-                }
+                char tmp = main[i];
+                int index = Array.IndexOf(chars, tmp);
+                res += keys[index];
             }
+
+            return res;
         }
 
+        public static string RevHcAlg(string res, string[] keys, char[] chars)
+        {
+            string main = "";
+            string tmp = "";
+            int i = 0;
+            while (i < res.Length)
+            {
+                tmp += res[i];
+                int ind = Array.IndexOf(keys, tmp);
+                if (ind != -1)
+                {
+                    main += chars[ind];
+                    tmp = "";
+                }
+
+                i++;
+            }
+
+            return main;
+        }
     }
 }
+
