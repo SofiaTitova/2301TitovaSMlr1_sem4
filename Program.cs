e0e852935718522bfceb3ab85a51b03db2b7a2d1
SofiaTitova <132710963+SofiaTitova@users.noreply.github.com>
2024-04-09 22:14:38 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,186 @@
-﻿using System;
-using System.Collections;
+﻿using bwt___mft__ha;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace burrows_wheeler_transform
+namespace bwt___mtf___AC
 {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string graf = "enwik7.txt"; //"graph.txt"
+            string graf_bwt = "bwt_graf.txt";
+            string graf_res = "resgraph.txt";
+            //bigBWT(graf, graf_bwt);
+            BWTFast bwt = new BWTFast();
+            string wholeText = "";
+            string line = "";
+            using (StreamWriter res = new StreamWriter(graf_bwt))
+            {
+                using (StreamReader reader = new StreamReader(graf, Encoding.UTF8))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        wholeText += line;
+
+                    }
+                }
+                bwt.GetBWT(wholeText);
+                res.Write(wholeText);
+            }
+            Console.WriteLine("+");
+
+            List<char> newAlphabet = new List<char>();
+            int index = 0;
+            line = "";
+            string result = "";
+
+            while (index < wholeText.Length)
+            {
+                int k = Math.Min(100, wholeText.Length - index);
+                line = wholeText.Substring(index, k);
+                newAlphabet = alphabet(line);
+                result += MTF(line, newAlphabet);
+                index += k;
+
+            }
+            Console.WriteLine("+");
+            int chunkSize = 10;
+            int j;
+            using (StreamWriter writer = new StreamWriter(graf_res))
+            {
+                for (j = 0; j < result.Length; j += chunkSize)
+                {
+                    string chunk = result.Substring(j, Math.Min(chunkSize, wholeText.Length - j));
+                    char[] chars2;
+                    double[] pos2;
+                    textAlphabet(chunk, out chars2, out pos2);
+
+                    double res2 = arifm_coding(chunk, pos2, chars2);
+                    string res = res2.ToString();
+                    res = res.Substring(2, res.Length - 2);
+                    long number = long.Parse(res); // Преобразование строки в целое число
+                    res = number.ToString("X");
+                    writer.Write(res);
+
+                }
+            }
+            double re = FileCompressionRatio.GetCompressionRatio(graf_res, graf);
+            Console.WriteLine(re);
+            Console.ReadKey();
+        }
+        static List<char> alphabet(string s)
+        {
+            List<char> chars = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!chars.Contains(s[i]))
+                {
+                    chars.Add(s[i]);
+                }
+            }
+            chars.Sort((a, b) => a.CompareTo(b));
+            return chars;
+        }
+
+        static string MTF(string s, List<char> alphabet)
+        {
+            string res = "";
+            int ind;
+            char symbol;
+            string integerVal;
+            char strEl;
+            char removedChar;
+            for (int i = 0; i < s.Length; i++)
+            {
+                symbol = s[i];
+                ind = alphabet.IndexOf(symbol);
+                integerVal = ind.ToString();
+                strEl = (char)(ind + 34);
+                res += strEl.ToString() /*integerVal*/;
+                removedChar = alphabet[ind];
+                alphabet.RemoveAt(ind);
+                alphabet.Insert(0, removedChar);
+            }
+            return res;
+        }
+        static double arifm_coding(string s, double[] p, char[] chars)
+        {
+            double[] intervals = new double[p.Length + 1];
+            double left_border = 0;
+            double right_border = 1;
+            double k = 0;
+            for (int i = 1; i < p.Length + 1; i++)
+            {
+                k += p[i - 1];
+                intervals[i] = k;
+            }
+            char toFind = ' ';
+            int index = 0;
+
+            foreach (char c in s)
+            {
+                double part_length = right_border - left_border;
+                toFind = c;
+                index = Array.IndexOf(chars, toFind);
+                left_border += intervals[index] * part_length;
+                right_border = left_border + p[index] * part_length;
+                if (left_border == right_border)
+                {
+                    break;
+                }
+            }
+            double res = (left_border + right_border) / 2;
+
+            return res;
+        }
+
+        static void textAlphabet(string s, out char[] chars, out double[] pos)
+        {
+            var alphabet = s.Distinct()
+                .OrderBy(c => (int)c)
+                .ToArray();
+            Array.Sort(alphabet);
+            chars = alphabet;
+            double[] freq = new double[chars.Length];
+            pos = new double[chars.Length];
+            int k = 0;
+            foreach (char ch in s)
+            {
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (ch == alphabet[i])
+                    {
+                        freq[i] += 1;
+                        k++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < chars.Length; i++)
+            {
+                pos[i] += freq[i] / k;
+            }
+        }
+    }
+    public class HeapNode
+    {
+        public char Sym { get; set; }
+        public int Val { get; set; }
+        public HeapNode LeftChild { get; set; }
+        public HeapNode RightChild { get; set; }
+
+        public HeapNode(char sym, int val)
+        {
+            Sym = sym;
+            Val = val;
+        }
+    }
+
     public class FileCompressionRatio
     {
         public static double GetCompressionRatio(string filePath1, string filePath2)
@@ -28,188 +200,5 @@ namespace burrows_wheeler_transform
 
             return ratio;
         }
-    }
-    internal class Program
-    {
-
-        public static int part = 50;
-        static void Main(string[] args)
-        {
-            string inputStr = "абракадабра";
-            int index = 0;
-            string result;
-            Console.WriteLine("Изначальная строка: {0}", inputStr);
-            BWT(out result, inputStr, ref index); 
-            Console.WriteLine("Строка после преобразования: {0}", result);
-            Console.WriteLine("Индекс строки: {0}", index);
-            string res = rBWT(result, index);
-            Console.WriteLine("Обратное преобразование: {0}", res);
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine();
-            string text = "text.txt";
-            string textRes = "textRes.txt";
-            string chb = "chb.txt";
-            string chbRes = "chbRes.txt";
-            string grey = "grey.txt";
-            string greyRes = "greyRes.txt";
-            bigBWT(text, textRes);
-            bigBWT(chb, chbRes);
-            bigBWT(grey, greyRes);
-            using (StreamReader reader = new StreamReader(text, Encoding.UTF8)) 
-            {
-                Console.WriteLine("текст до BWT: {0}", strLen(reader));
-            }
-            using (StreamReader reader = new StreamReader(textRes, Encoding.UTF8)) 
-            {
-                Console.WriteLine("текст после BWT: {0}", strLen(reader));
-            }
-            Console.WriteLine();
-            using (StreamReader reader = new StreamReader(chb, Encoding.UTF8)) 
-            {
-                Console.WriteLine("чб до BWT: {0}", strLen(reader));
-            }
-            using (StreamReader reader = new StreamReader(chbRes, Encoding.UTF8)) 
-            {
-                Console.WriteLine("чб после BWT: {0}", strLen(reader));
-            }
-            Console.WriteLine();
-            using (StreamReader reader = new StreamReader(grey, Encoding.UTF8)) 
-            {
-                Console.WriteLine("оттенки серого до BWT: {0}", strLen(reader));
-            }
-            using (StreamReader reader = new StreamReader(greyRes, Encoding.UTF8)) 
-            {
-                Console.WriteLine("оттенки серого после BWT: {0}", strLen(reader));
-            }
-
-           
-            Console.ReadKey();
-
-        }
-        static double strLen(StreamReader reader)
-        {
-            string line;
-            double k = 0;
-            double part = 0;
-            double j = 0;
-            double len = 0;
-            while ((line = reader.ReadLine()) != null)
-            {
-                len += line.Length;
-                for (int i = 0; i < line.Length - 1; i++)
-                {
-                    if (line[i] == line[i + 1])
-                    {
-                        k++;
-                        if (j == 0)
-                        {
-                            part++;
-                            j++;
-                        }
-                    }
-                    else
-                    {
-                        j = 0;
-                    }
-                }
-            }
-            k = k + part;
-            double lengthStr;
-            lengthStr = (k - 2 * part) / len;
-            return lengthStr;
-        }
-        static void BWT(out string result, string inStr, ref int ind)
-        {
-            result = "";
-            string[] bwt_matrix = new string [inStr.Length] ;
-            string toAdd;
-            string mainPart;
-            string newStr = inStr;
-            string part;
-            int len = newStr.Length;
-            for (int i = 0; i < len; i++)
-            {
-                toAdd = inStr.Substring(0, 1);
-                mainPart = inStr.Substring(1, len - 1);
-                inStr = mainPart + toAdd;
-                bwt_matrix[i] = inStr;
-
-            }
-            string[] sorted_bwt = bwt_matrix.OrderBy(word => word).ToArray();
-            for (int i = 0; i < len; i++)
-            {
-                if (sorted_bwt[i] == newStr)
-                {
-                    ind = i;
-                }
-            }
-            for (int i = 0; i < len; i++)
-            {
-                part = sorted_bwt[i].Substring(len - 1);
-                result += part;
-            }
-
-        }
-        static string rBWT(string result, int ind)
-        {
-            int len = result.Length;
-            string[] bwt_matrix = new string[len];
-            for (int i = 0; i < len; i++)
-            {
-                bwt_matrix[i] = string.Empty; 
-            }
-            for (int j = 0; j < len - 1; j++)
-            {
-                for (int i = 0; i < len; i++)
-                {
-                    bwt_matrix[i] = result[i] + bwt_matrix[i];
-                }
-                bwt_matrix = bwt_matrix.OrderBy(bwt => bwt).ToArray();
-            }
-            for (int i = 0; i < len; i++)
-            {
-                bwt_matrix[i] = bwt_matrix[i] + result[i];
-            }
-            return bwt_matrix[ind];
-        }
-
-        static void bigBWT(string inp, string outp)
-        {
-            string line;
-            using (StreamReader reader = new StreamReader(inp, Encoding.UTF8))  
-            using (StreamWriter writer = new StreamWriter(outp))                 
-            {
-                while ((line = reader.ReadLine()) != null)
-                {
-                    int len = line.Length;          
-                    int k = 0;                    
-
-                    for (int i = len; i > 0; i -= part){
-                        string textPart;           
-                        int partLen;      
-
-                        if (i - part < 0) 
-                        {
-                            textPart = line.Substring(part * k);                         
-                            partLen = line.Length - part * k;   
-                        }
-                        else
-                        {
-                            textPart = line.Substring(part * k, part);       
-                            k++;
-                            partLen = part;                                
-                        }
-                        string result;
-                        int ind = 0;
-                        BWT(out result, textPart, ref ind);
-                        writer.Write(result);           
-                        writer.Write((char)(ind + part));     
-
-                    }
-                    writer.Write("\n");  
-                }
-            }
-        }
-
     }
 }
---
+++ bwt.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bwt___mft__ha
+{
+    public class Tuple : IComparable<Tuple>
+    {
+        public int OriginalIndex { get; set; }  // хранит оригинальный индекс суффикса
+        public int FirstHalf { get; set; }      // хранит ранг первой половины суффикса
+        public int SecondHalf { get; set; }     // хранит ранг второй половины суффикса
+
+        public int CompareTo(Tuple other)
+        {
+            if (this.FirstHalf == other.FirstHalf)
+            {
+                return this.SecondHalf.CompareTo(other.SecondHalf);
+            }
+            else
+            {
+                return this.FirstHalf.CompareTo(other.FirstHalf);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Tuple{{OriginalIndex={OriginalIndex}, FirstHalf={FirstHalf}, SecondHalf={SecondHalf}}}";
+        }
+    }
+
+    public class BWTFast
+    {
+        public StringBuilder GetBWT(string s)
+        {
+            s += "$";
+            StringBuilder bwt = new StringBuilder();
+            int[] suffixArray = Compress(s);
+            foreach (int i in suffixArray)
+            {
+                int j = i - 1;
+                if (j < 0)
+                {
+                    j += suffixArray.Length;
+                }
+                bwt.Append(s[j]);
+            }
+            return bwt;
+        }
+
+        public int[] Compress(string s)
+        {
+            int N = s.Length;
+
+            int steps = (int)Math.Log(N, 2);
+
+            int[][] rank = new int[steps + 1][];
+            for (int i = 0; i <= steps; i++)
+            {
+                rank[i] = new int[N];
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                rank[0][i] = s[i] - 'a';
+            }
+
+            Tuple[] tuples = new Tuple[N];
+
+            for (int step = 1, cnt = 1; step <= steps; step++, cnt <<= 1)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    Tuple tuple = new Tuple();
+                    tuple.FirstHalf = rank[step - 1][i];
+                    tuple.SecondHalf = i + cnt < N ? rank[step - 1][i + cnt] : -1;
+                    tuple.OriginalIndex = i;
+
+                    tuples[i] = tuple;
+                }
+
+                Array.Sort(tuples);
+
+                rank[step][tuples[0].OriginalIndex] = 0;
+
+                for (int i = 1, currRank = 0; i < N; i++)
+                {
+                    if (tuples[i - 1].FirstHalf != tuples[i].FirstHalf || tuples[i - 1].SecondHalf != tuples[i].SecondHalf)
+                    {
+                        ++currRank;
+                    }
+                    rank[step][tuples[i].OriginalIndex] = currRank;
+                }
+            }
+
+            int[] suffixArray = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                suffixArray[i] = tuples[i].OriginalIndex;
+            }
+
+            return suffixArray;
+        }
+    }
+}
