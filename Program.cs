76ce3d3875a2305a462553ee032cd413c4814ad0
SofiaTitova <132710963+SofiaTitova@users.noreply.github.com>
2024-04-09 22:25:39 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,98 +1,309 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+﻿
+using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.IO;
+using System.Net.Mail;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using System.Xml.Linq;
 
-namespace mtf
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Начальная строка");
-            string newStr = "bcabaaa";
-            Console.WriteLine(newStr);
-            List<char> newAlphabet = new List<char>();
-            newAlphabet = alphabet(newStr);
-            string result = "";
-            string result2 = "";
-            result = MTF(newStr, newAlphabet);
-            Console.WriteLine("Преобразованная строка");
-            Console.WriteLine(result);
-            Console.ReadKey();
-            result2 = reverse_MTF(result, newAlphabet);
-            Console.WriteLine("Обратное преобразование");
-            Console.WriteLine(result2);
-            Console.ReadKey();
-        }
+        string image = "output2.txt"; // файл с изображением 
+        string result = "convertRes.txt"; //сжатие чб
+        string toImage = "output_new.txt"; //разжатие чб
+        string greyImage = "greyShades.txt"; // файл в оттенках   "greyShades_check.txt"
+        string toGreyImage = "greyResult.txt"; //сжатие серого
+        string greyRes = "greyConvertResult.txt"; //разжатие серого
+        string colorImage = "colorShades.txt"; //"colorShades_check.txt";
+        string toColorImage = "colorResult.txt"; //сжатие цветного
+        string colorRes = "colorConvertResult.txt"; //разжатие цветного
 
-        static List<char> alphabet(string s){
-            List<char> chars = new List<char>();
-            for (int i = 0; i < s.Length; i++)
+        Console.WriteLine("Выберите оформат изображения:");
+        Console.WriteLine("1. Черно - белое");
+        Console.WriteLine("2. Оттенки серого");
+        Console.WriteLine("3. Цветное");
+        int choice = 0;
+        choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            if (!File.Exists(image)) // проверка существования
             {
-                if (!chars.Contains(s[i]))
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+
+            using (StreamReader reader = new StreamReader(image)) //чтение построчно
+            {
+                using (StreamWriter res = new StreamWriter(result)) //запись в файл
                 {
-                    chars.Add(s[i]);
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        char prevChar = line[0];
+                        int count = 1;
+                        if (prevChar == '1') // всегжа начинается с 0
+                        {
+                            count = 0;
+                            prevChar = '0';
+                            res.Write((char)(count));
+
+                            prevChar = line[0];
+                            count = 1;
+                        }
+
+                        for (int i = 1; i < line.Length; i++) //записываем кол-во подряд идущих
+                        {
+                            if (line[i] == prevChar)
+                            {
+                                count++;
+                            }
+                            else
+                            {
+                                res.Write((char)(count));
+
+                                prevChar = line[i];
+                                count = 1;
+                            }
+                        }
+                        res.Write((char)(count));
+                        res.Write('\n');
+                    }
                 }
             }
-            chars.Sort((a, b) => a.CompareTo(b));
-            return chars;
+
+            using (StreamReader reader = new StreamReader(result)) //чтение построчно
+            {
+                using (StreamWriter toImg = new StreamWriter(toImage)) //запись разжатия
+                {
+                    string line;
+                    char charecter = '0';
+                    while ((line = reader.ReadLine()) != null) //считываем построчно
+                    {
+                        if (line[0] == '0') // так как всегда в начале ноль
+                        {
+                            charecter = '1';
+                        }
+                        else
+                        {
+                            charecter = '0';
+                        }
+
+                        for (int i = 0; i < line.Length; i++) // выписываем 0 и 1
+                        {
+                            int count = (int)(line[i]);
+
+                            for (int j = 0; j < count; j++)
+                            {
+                                toImg.Write(charecter);
+                            }
+
+                            if (charecter == '0') // чтобы чередовать
+                            {
+                                charecter = '1';
+                            }
+                            else
+                            {
+                                charecter = '0';
+                            }
+                        }
+                        toImg.Write('\n');
+                    }
+                }
+            }
+            checker(result, image);
+        }
+        else if (choice == 2)
+        {
+            if (!File.Exists(greyImage)) // проверка существования
+            {
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+
+
+
+            using (StreamReader reader = new StreamReader(greyImage, Encoding.UTF8)) //чтение построчно
+            {
+                using (StreamWriter res = new StreamWriter(greyRes))
+                {
+                    int cur;
+                    int k = 0;
+                    char prevChar = '\0';
+                    int count = 1;
+                    while ((cur = reader.Read()) != -1)
+                    {
+                        k++;
+                        char ch = (char)cur;
+
+
+                        if (ch == prevChar)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            if (prevChar != '\0')
+                            {
+                                res.Write((char)(count));
+                                res.Write(prevChar);
+                            }
+                            prevChar = ch;
+                            count = 1;
+                        }
+
+                    }
+                    res.Write((char)(count));
+                    res.Write(prevChar);
+                }
+            }
+
+            using (StreamReader reader = new StreamReader(greyRes)) //чтение построчно
+            {
+                using (StreamWriter togrImg = new StreamWriter(toGreyImage))
+                {
+                    int cur;
+                    int k = 0;
+
+                    int count = 1;
+                    char ch = '\0';
+                    while ((cur = reader.Read()) != -1)
+                    {
+                        if (k > 0)
+                        {
+                            ch = (char)cur;
+                        }
+                        else
+                        {
+                            count = cur;
+                        }
+                        if (k > 0)
+                        {
+                            for (int j = 0; j < count; j++)
+                            {
+                                togrImg.Write(ch);
+
+                            }
+                            k = 0;
+                        }
+                        else k++;
+                    }
+                }
+            }
+            checker(greyRes, greyImage);
         }
 
-        static string MTF(string s, List<char> alphabet)
+        else if (choice == 3)
         {
-            string res = "";
-            int ind;
-            char symbol;
-            string integerVal;
-            char strEl;
-            char removedChar;
-            for (int i = 0; i < s.Length; i++)
+            if (!File.Exists(colorImage)) // проверка существования
             {
-                symbol = s[i];
-                ind = alphabet.IndexOf(symbol);
-                integerVal = ind.ToString();
-                strEl = (char)(ind+34);
-                res += strEl.ToString() /*integerVal*/;
-                removedChar = alphabet[ind];
-                alphabet.RemoveAt(ind);
-                alphabet.Insert(0, removedChar);
+                Console.WriteLine("Файл не найден.");
+                return;
             }
-            return res;
-        }
-        static string reverse_MTF(string s, List<char> alphabet)
-        {
-            string res = "";
-            int ind;
-            char removedChar;
 
-            int[] s_inInt = new int[s.Length];
-            s_inInt = toArr(s);
+            using (StreamReader reader = new StreamReader(colorImage, Encoding.UTF8)) //чтение построчно
+            {
+                using (StreamWriter res = new StreamWriter(colorRes))
+                {
+                    int cur;
+                    int k = 0;
+                    char prevChar = '\0';
+                    int count = 1;
+                    char ch1;
+                    char ch2;
+                    char ch3;
+                    string line = "";
+                    while ((cur = reader.Read()) != -1)
+                    {
+                        ch1 = (char)cur;
+                        line += ch1;
+                    }
+                    int i = 0;
+                    int i3 = 3;
 
-            for (int i = 0; i < s.Length; i++)
-            {
-                res += alphabet[s_inInt[i]];
-                ind = s_inInt[i];
-                removedChar = alphabet[ind];
-                alphabet.RemoveAt(ind);
-                alphabet.Insert(0, removedChar);
+                    while (i < line.Length - 6)
+                    {
+                        if ((line[i] == line[i + i3]) && (line[i + 1] == line[i + i3 + 1]) && (line[i + 2] == line[i + i3 + 2]))
+                        {
+                            count++;
+                            i += 3;
+                        }
+                        else
+                        {
+                            res.Write((char)(count));
+                            res.Write(line[i]);
+                            res.Write(line[i + 1]);
+                            res.Write(line[i + 2]);
+                            count = 1;
+                            i += 3;
+                        }
+                    }
+                    res.Write((char)(count));
+                    res.Write(line[i]);
+                    res.Write(line[i + 1]);
+                    res.Write(line[i + 2]);
+                }
             }
-            return res;
+
+            using (StreamReader reader = new StreamReader(colorRes)) //чтение построчно
+            {
+                using (StreamWriter toImg = new StreamWriter(toColorImage))
+                {
+                    int cur;
+                    int k = 0;
+                    char ch1;
+                    string line = "";
+                    while ((cur = reader.Read()) != -1)
+                    {
+                        ch1 = (char)cur;
+                        line += ch1;
+                    }
+                    int count = 1;
+                    int i = 0;
+                    while (i < line.Length - 3)
+                    {
+                        k = (int)line[i];
+                        for (int j = 0; j < k; j++)
+                        {
+                            toImg.Write(line[i + 1]);
+                            toImg.Write(line[i + 2]);
+                            toImg.Write(line[i + 3]);
+                        }
+                        i += 4;
+                    }
+                }
+            }
+            checker(colorRes, colorImage);
         }
-        static int[] toArr (string s)
+    }
+
+
+    static void checker(string compr, string decompr)
+    {
+
+        double fileSize1 = new FileInfo(compr).Length;
+        double fileSize2 = new FileInfo(decompr).Length;
+
+        if (fileSize1 > fileSize2)
         {
-            int[] num = new int[s.Length];
-            int k = 0;
-            for(int i = 0; i < s.Length; i++)
-            {
-                k = (int)s[i] - 34;
-                num[i] = k;
-            }
-            return num;
+            Console.WriteLine("Размер СЖАТОГО файла: {0}\nРазмер ИСХОДНОГО файла: {1}", fileSize1, fileSize2);
+            Console.WriteLine("Мощность сжатия: {0}", fileSize2 / fileSize1);
+
         }
+        else if (fileSize1 < fileSize2)
+        {
+            Console.WriteLine("Размер СЖАТОГО файла: {0}\nРазмер ИСХОДНОГО файла: {1}", fileSize1, fileSize2);
+            Console.WriteLine("Мощность сжатия: {0}", fileSize2 / fileSize1);
+        }
+        else
+        {
+            Console.WriteLine("Размер СЖАТОГО файла: {0}\nРазмер ИСХОДНОГО файла: {1}", fileSize1, fileSize2);
+            Console.WriteLine("Мощность сжатия: {0}", fileSize2 / fileSize1);
+        }
+
+        Console.ReadLine();
     }
 }
